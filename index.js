2c6e7aba762514fd26bf0c21def0879099817b7e
seonwoo kim <nieguy61@eoding.com>
2023-10-16 14:22:05 +0900

    add new code

--- index.js
+++ index.js
@@ -6,6 +6,21 @@ require('dotenv').config();
 const app = express();
 const port = 3000; // 원하는 포트 번호로 변경
 
+function formatDateFromNotion(notionDate) {
+  const date = new Date(notionDate);
+
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0'); // 월은 0부터 시작하므로 +1
+  const day = String(date.getDate()).padStart(2, '0');
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  const seconds = String(date.getSeconds()).padStart(2, '0');
+
+  const formattedDate = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+
+  return formattedDate;
+}
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Slack 커맨드 처리
@@ -19,7 +34,8 @@ app.post('/', async (req, res) => {
     // Slack에 응답 보내기
     const slackResponse = {
       response_type: 'in_channel', // 'in_channel' 또는 'ephemeral' (비공개 응답) 설정 가능
-      text: '검색 결과: ' + notionResponse,
+      text: '검색 결과:',
+      attachments: notionResponse, // 검색 결과를 attachments에 추가
     };
     res.json(slackResponse);
   } catch (error) {
@@ -42,32 +58,42 @@ async function searchInNotion(query) {
 
   const requestBody = {
     filter: {
-      property: 'title',
+      property: 'Task',
       text: {
         contains: query,
       }
     },
   };
-  console.log(requestBody)
 
   try {
     const response = await axios.post(notionApiUrl, requestBody, { headers });
     const results = response.data.results;
-    console.log(JSON.stringify(results))
+    
     if (results.length > 0) {
       // 검색 결과 처리
       const formattedResults = results.map((item) => {
-        const title = item.properties.Title.rich_text[0].plain_text;
+        const title = item.properties.Task.title[0].plain_text;
         const url = item.url; // Notion 문서의 URL
-      
-        return `<${url}|${title}>`; // 링크된 문서 제목 생성
+        const createdBy = item.properties['생성자'].created_by.name;
+        const status = item.properties['상태'].status.name;
+        const taskID = item.properties['Task-id'].unique_id.prefix + "-" + item.properties['Task-id'].unique_id.number;
+        const createdTime = formatDateFromNotion(item.properties['생성 일시'].created_time);
+
+        return {
+          title: title,
+          title_link: url,
+          fields: [
+            { title: '생성자', value: createdBy, short: true },
+            { title: '상태', value: status, short: true },
+            { title: 'Task ID', value: taskID, short: true },
+            { title: '생성 일시', value: createdTime, short: true }
+          ],
+        };
       });
       
-      const response = formattedResults.join(', ');
-      // return results.map((item) => item.properties.Title.rich_text[0].plain_text).join(', ');
-      return response;
+      return formattedResults;
     } else {
-      return '검색 결과 없음';
+      return [{ text: '검색 결과 없음' }];
     }
   } catch (error) {
     throw error;
